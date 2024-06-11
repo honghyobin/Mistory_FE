4aa06aaa617665f8e4e6911535569a37f8b29cce
honghyobin <w2234@e-mirim.hs.kr>
2024-06-11 09:45:51 +0900

    손전등 css 추가

--- Mistory/js/dark-mood.js
+++ Mistory/js/dark-mood.js
@@ -19,4 +19,5 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Dark mode setting after toggle:', localStorage.getItem('darkMode')); // 토글 후 로컬 스토리지 값 확인
         console.log('Body class list:', document.body.classList); // 클래스 목록 확인
     });
+    
 });
